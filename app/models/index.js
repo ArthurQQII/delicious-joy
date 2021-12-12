c711822945629b4df0ddd12c82fc1dde03902fa1
chanqihui <91418311+chanqihui@users.noreply.github.com>
2021-12-12 23:43:47 +1000

    model added
    model

--- app/models/index.js
+++ app/models/index.js
@@ -1,11 +1,10 @@
 const dbConfig = require("../config/db.config.js");
 
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.test = require("../models/test.model.js")(mongoose);
+db.user = require("./user.model.js")(mongoose);
 
 module.exports = db;
